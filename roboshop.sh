c2928e93e40ad8767794bc77817b756b3836b51c
suresh228devops <suresh228devops@gmail.com>
2025-05-20 20:36:53 +0530

    updated roboshop.sh file

--- roboshop.sh
+++ roboshop.sh
@@ -6,9 +6,10 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z10033673H6OAQSF2Q3OH" # replace with your ZONE ID
 DOMAIN_NAME="ksrdaws84s.site" # replace with your domain
 
+
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids g-0a06afa30bce69f67 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a06afa30bce69f67 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -17,8 +18,7 @@ do
     fi
     echo "$instance IP address: $IP"
 
-
-aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
@@ -35,5 +35,4 @@ aws route53 change-resource-record-sets \
         }
         }]
     }'
-
 done
