a2dc62836d1235470f84bf2d8c5b7aaac955353a
suresh228devops <suresh228devops@gmail.com>
2025-05-25 21:45:33 +0530

    commited on 25/05/25

--- user-set.sh
+++ user-set.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+set -e
+
+failure(){
+  echo "Failed at ::$1 $2"
+}
+
+trap 'failure "${LINENO}" "${BASH_COMMAND}" ' ERR
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
@@ -22,58 +30,31 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs:20"
-
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
 
 id roboshop
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
 else
     echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading user"
 
 rm -rf /app/*
 cd /app 
 unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "unzipping user"
-
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
 
 cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
-VALIDATE $? "Copying user service"
-
 systemctl daemon-reload &>>$LOG_FILE
 systemctl enable user  &>>$LOG_FILE
 systemctl start user
-VALIDATE $? "Starting user"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
